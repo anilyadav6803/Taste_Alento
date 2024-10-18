d3e85b462e8c1d01e303ffae48304137012ae09a
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-10-19 01:46:37 +0530

    F packages

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/UI/button'
+
+function App() {
+
+
+  return (
+    <div>
+         <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
