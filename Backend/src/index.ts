ccc79d7baec6277c4dd3e4ea1d5e834e888c146d
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-10-19 00:03:31 +0530

    index file

---
+++ Backend/src/index.ts
@@ -0,0 +1,29 @@
+
+import cors from "cors";
+import express, { Request, Response } from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
+
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
+});
