978af39365813da8385fd2e4e2111912833e3eff
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-10-22 23:27:23 +0530

    hero part

--- Frontend/src/components/Hero.tsx
+++ Frontend/src/components/Hero.tsx
@@ -1,9 +1,13 @@
-import hero from '../assets/hero.png'
+import hero from '../assets/hero.png';
 
 export default function Hero() {
   return (
     <div>
-       <img src={hero} className='w-full max-h-[600px] object-cover'/>
+      <img 
+        src={hero} 
+        alt="Hero section" 
+        className="w-full max-h-[600px] object-cover" 
+      />
     </div>
-  )
+  );
 }
