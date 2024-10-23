997ceb54c7c3c097fd7be76d479883d70b47203e
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-10-23 22:20:02 +0530

    layout

--- Frontend/src/layouts/layout.tsx
+++ Frontend/src/layouts/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Footer from '@/components/Footer'
 type Props = {
   children: React.ReactNode
 }
@@ -10,7 +11,10 @@ function Layout({ children }: Props) {
     <div className='flex flex-col min-h-screen'>
       <Header/>
       <Hero/>
+     
       <div className='container mx-auto flex-1 py-10'>{children}</div><div className='h-20'></div>
+
+      <Footer/>
     </div>
   )
 }
