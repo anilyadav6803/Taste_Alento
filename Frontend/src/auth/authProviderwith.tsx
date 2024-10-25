86eef592f413a06d8585d91d77a77fd6335f5eec
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-10-25 23:48:01 +0530

    mini change

--- Frontend/src/auth/authProviderwith.tsx
+++ Frontend/src/auth/authProviderwith.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { AppState, Auth0Provider } from '@auth0/auth0-react';
-import { on } from 'events';
+import React from "react";
+import { AppState, Auth0Provider } from "@auth0/auth0-react";
+
+
 
 type Props = {
   children: React.ReactNode;
@@ -8,19 +9,26 @@ type Props = {
 
 const AuthProviderWith: React.FC<Props> = ({ children }) => {
   // Add authentication logic here if needed
-  const domain = import.meta.env.VITE_AUTH0_DOMAIN; 
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
   if (!domain || !clientId || !audience) {
-    throw new Error('Missing environment variables');
+    throw new Error("Missing environment variables");
   }
 
-  const onRedirectCallback = (appState: AppState , user ?: User) => {
-      
-  }
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+   console.log('User', user); 
+  };
   return (
-   <Auth0Provider domain={domain} clientId={clientId} authorizationParams={{ audience }}>{children} onRedirectCallback = {onRedirectCallback} </Auth0Provider>
-   
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{ redirect_uri: redirectUri }}
+      onRedirectCallback = {onRedirectCallback}
+    >
+      {children}
+    
+    </Auth0Provider>
   );
 };
 
