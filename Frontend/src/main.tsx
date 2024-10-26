618f1ae8ac2daca7795ebe83d184b0939a9827c3
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-10-26 23:51:53 +0530

    main file

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import AppRoutes from './AppRoutes.tsx'
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes.tsx";
+import AuthProviderWithAuth0 from "./auth/AuthProviderWithAuth0.tsx";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-   <Router>
-    <AppRoutes/>
-   </Router>
-  </StrictMode>,
-)
+    <Router>
+      <AuthProviderWithAuth0>
+        <AppRoutes />
+      </AuthProviderWithAuth0>
+    </Router>
+  </StrictMode>
+);
