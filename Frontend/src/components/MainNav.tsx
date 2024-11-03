2e7a81ba8fa7542e106bd6b97d508b930caa6464
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-03 23:54:06 +0530

    mainnav

--- Frontend/src/components/MainNav.tsx
+++ Frontend/src/components/MainNav.tsx
@@ -1,15 +1,24 @@
 import { Button } from "./UI/button";
 import { useAuth0 } from "@auth0/auth0-react";
+import UsernameMenu from "./UsernameMenu";
 
 export default function MainNav() {
-  const  {loginWithRedirect} = useAuth0()
+  const  {loginWithRedirect , isAuthenticated} = useAuth0()
   return (
-   <Button 
-   variant={"ghost"}
-   className='font-bold hover:text-orange-500 lg: pr-20 hover:bg-white'
-   onClick={async() => await loginWithRedirect({})}
-    >
-    Log In
-   </Button>
+    <div>
+      <span className="flex space-x-2 item-center"> 
+        {isAuthenticated ? (<UsernameMenu/> ) : (
+          <Button 
+          variant={"ghost"}
+          className='font-bold hover:text-orange-500 lg: pr-20 hover:bg-white'
+          onClick={async() => await loginWithRedirect({})}
+           >
+           Log In
+          </Button>
+        ) }
+      </span>
+   
+    </div>
+ 
   )
 }
