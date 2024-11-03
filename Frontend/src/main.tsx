6a1053cefbae29e4374364b9e003e57d6bebebd9
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-03 23:53:46 +0530

    main

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,10 +4,20 @@ import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes.tsx";
 import AuthProviderWithAuth0 from "./auth/AuthProviderWithAuth0.tsx";
+
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_REDIRECT_URI;
+
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <AuthProviderWithAuth0>
+      <AuthProviderWithAuth0
+        domain={domain}
+        clientId={clientId} 
+        redirectUri= {redirectUri}
+      >
         <AppRoutes />
       </AuthProviderWithAuth0>
     </Router>
