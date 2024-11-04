632526e3076e0ecd97a5362239e7fa391cd0045e
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-04 23:59:15 +0530

    update auth0

--- Frontend/src/auth/AuthProviderWithAuth0.tsx
+++ Frontend/src/auth/AuthProviderWithAuth0.tsx
@@ -1,39 +1,39 @@
-import React from "react";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { AppState, Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const AuthProviderWithAuth0: React.FC<Props> = ({ children }) => {
+const AuthProviderWithAuth0 = ({ children }: Props) => {
+  const navigate = useNavigate();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_REDIRECT_URI;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId || !redirectUri) {
-    throw new Error("Missing environment variables");
+  if (!domain || !clientId || !redirectUri || !audience) {
+    throw new Error("unable to initialise auth");
   }
 
   const onRedirectCallback = (appState?: AppState) => {
-    window.history.replaceState({}, document.title, appState?.returnTo || "/"); // Redirect to home if returnTo is undefined
+    navigate(appState?.returnTo || "/auth-callback");
   };
-  
 
-  try {
-    return (
-      <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        authorizationParams={{ redirect_uri: redirectUri }}
-        onRedirectCallback={onRedirectCallback}
-      >
-        {children}
-      </Auth0Provider>
-    );
-  } catch (error) {
-    console.error("Error initializing Auth0:", error);
-    return <div>Authentication service is currently unavailable. Please try again later.</div>;
-  }
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+        audience,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
 };
 
 export default AuthProviderWithAuth0;
