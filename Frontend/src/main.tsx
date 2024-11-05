c37ca804889172db1ec8811e56a10d83f863bd97
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-05 23:34:51 +0530

    main file update

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,25 +1,22 @@
-import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes.tsx";
-import AuthProviderWithAuth0 from "./auth/AuthProviderWithAuth0.tsx";
 
-const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_REDIRECT_URI;
-
+import { Auth0Provider } from "@auth0/auth0-react";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Router>
-      <AuthProviderWithAuth0
-        domain={domain}
-        clientId={clientId} 
-        redirectUri= {redirectUri}
-      >
-        <AppRoutes />
-      </AuthProviderWithAuth0>
-    </Router>
-  </StrictMode>
+  <Router>
+  
+  <Auth0Provider
+    domain="webcodes.us.auth0.com"
+    clientId="TycumvVaZDDKWuHlmOLaPEFlkQUgOsaj"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <AppRoutes />
+  </Auth0Provider>
+  </Router>
 );
