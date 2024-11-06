789fc6a3e0157c93f615fa0808f3c23830a32334
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-06 23:56:11 +0530

    index backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,6 +5,8 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import { v2 as cloudinary } from "cloudinary";
 
+import MyUserRoute from "./routes/MyUserRoutes";
+
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -20,9 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req: Request, res: Response) => {
-  res.send({ message: "health OK!" });
-});
+app.use("/api/my/user" , MyUserRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
