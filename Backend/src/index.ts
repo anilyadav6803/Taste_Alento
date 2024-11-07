48ac04aaf6095b724b61c64ce49f4072f462d070
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-07 22:27:21 +0530

    index

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -22,7 +22,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.use("/api/my/user" , MyUserRoute);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
