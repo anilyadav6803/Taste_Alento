628d76072c8ae85054028142da387f879512d3e6
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-07 22:27:50 +0530

    main

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,12 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes.tsx";
+import { QueryClient } from "react-query";
+
+import { QueryClientProvider } from "react-query";
 
 import { Auth0Provider } from "@auth0/auth0-react";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <Router>
-  
+  <QueryClientProvider client={queryClient} >
   <Auth0Provider
     domain="webcodes.us.auth0.com"
     clientId="TycumvVaZDDKWuHlmOLaPEFlkQUgOsaj"
@@ -18,5 +28,6 @@ createRoot(document.getElementById("root")!).render(
   >
     <AppRoutes />
   </Auth0Provider>
+  </QueryClientProvider>
   </Router>
 );
