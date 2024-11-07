e9875a1eb9c3f4f560482c28a109c69da4a1b40b
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-07 22:28:09 +0530

    auth

--- Frontend/src/auth/AuthProviderWithAuth0.tsx
+++ Frontend/src/auth/AuthProviderWithAuth0.tsx
@@ -1,6 +1,7 @@
-import { Auth0Provider, AppState } from "@auth0/auth0-react";
+import { Auth0Provider, AppState, User } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 import React from "react";
+import { useCreateMyUser } from "../api/MyUserApi";
 
 type AuthProviderWithAuth0Props = {
   children: React.ReactNode;
@@ -10,6 +11,7 @@ const AuthProviderWithAuth0 = ({ children }: AuthProviderWithAuth0Props) => {
   const navigate = useNavigate();
 
   // Load Auth0 environment variables
+  const { createUser } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_REDIRECT_URI || window.location.origin; // Fallback to current origin if undefined
@@ -21,8 +23,18 @@ const AuthProviderWithAuth0 = ({ children }: AuthProviderWithAuth0Props) => {
   }
 
   // Redirect callback function
-  const onRedirectCallback = (appState?: AppState) => {
-    navigate(appState?.returnTo || "/");
+  const onRedirectCallback = async (appState?: AppState, user?: User) => {
+    // Check if user object has the required properties
+    if (user?.sub && user?.email) {
+      // Call createUser mutation asynchronously
+      await createUser({
+        auth0Id: user.sub,
+        email: user.email,
+      });
+    }
+
+    // Redirect to the target URL after login, if specified
+    navigate(appState?.returnTo || window.location.pathname);
   };
 
   return (
