c0ddd5d17a9ca6c09c39ec4e6fec31768cea504b
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-14 23:52:22 +0530

    userprofile

---
+++ Frontend/src/components/pages/UserPofile.tsx
@@ -0,0 +1,10 @@
+import UserProfileForm from "@/form/user-profile-form/UserProfileForm";
+
+
+export default function UserPofile() {
+  return (
+   <div>
+     <UserProfileForm/>;
+   </div>
+  )
+}
