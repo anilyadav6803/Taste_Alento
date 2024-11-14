aae907b01a4b9bfef9c1cb3a4bf6e3375d00e4fa
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-14 23:52:08 +0530

    appproutes

--- Frontend/src/AppRoutes.tsx
+++ Frontend/src/AppRoutes.tsx
@@ -3,14 +3,16 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './layouts/layout'
 import Homepage from './components/pages/Homepage'
 import AuthCallbackPage from './components/pages/AuthCallback'
+import UserProfile from './components/pages/UserPofile'
 const AppRoutes = () => {
   return (
     <Routes>
 
         <Route path="/*" element={<Layout><Homepage/></Layout>} />
         <Route path = "/auth-callback" element={<AuthCallbackPage/> }></Route>
-        <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
+        <Route path="/user-profile" element={<Layout showHero><UserProfile/></Layout>} />
         <Route path="*" element={<Navigate to='/'/>} />
+
     </Routes>
   )
 }
