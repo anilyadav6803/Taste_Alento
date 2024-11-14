8a6d0563a1fc6d6a7393ee824c02989b3a24b04d
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-14 23:52:32 +0530

    loadinggbutton

---
+++ Frontend/src/components/LoadingButton.tsx
@@ -0,0 +1,15 @@
+
+import { Button } from './UI/button'
+
+
+import { Loader2 } from 'lucide-react'
+
+export default function LoadingButton() {
+    
+  return (
+    <Button disabled>
+       <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        </Button>
+
+  )
+}
