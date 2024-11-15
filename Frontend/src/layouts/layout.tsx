496e0bc4098262be56306850007b44e0ea7b2ae2
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-15 23:20:39 +0530

    layout

--- Frontend/src/layouts/layout.tsx
+++ Frontend/src/layouts/layout.tsx
@@ -5,21 +5,17 @@ import Footer from '@/components/Footer'
 type Props = {
   children: React.ReactNode;
   showHero?: boolean;
+};
 
-}
-
-function Layout({ children , showHero = false }: Props) {
+const Layout = ({ children, showHero = false }: Props) => {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <Header/>
-      {showHero && <Hero/>}
-      
-     
-      <div className='container mx-auto flex-1 py-10'>{children}</div><div className='h-20'></div>
-
-      <Footer/>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      {showHero && <Hero />}
+      <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
