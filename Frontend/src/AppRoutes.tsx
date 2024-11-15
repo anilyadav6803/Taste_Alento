9639808caf6a4e1f4b00201ef11ca216c51f5887
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-15 23:20:01 +0530

    approute

--- Frontend/src/AppRoutes.tsx
+++ Frontend/src/AppRoutes.tsx
@@ -1,20 +1,44 @@
-
-import { Navigate, Route, Routes } from 'react-router-dom'
-import Layout from './layouts/layout'
-import Homepage from './components/pages/Homepage'
-import AuthCallbackPage from './components/pages/AuthCallback'
-import UserProfile from './components/pages/UserPofile'
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/layout";
+import Homepage from "./components/pages/Homepage";
+import AuthCallbackPage from "./components/pages/AuthCallback";
+import UserProfile from "./components/pages/UserPofile";
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            <Homepage />
+          </Layout>
+        }
+      />
+      <Route
+       path="/auth-callback" 
+       element={<AuthCallbackPage />}
+        />
 
-        <Route path="/*" element={<Layout><Homepage/></Layout>} />
-        <Route path = "/auth-callback" element={<AuthCallbackPage/> }></Route>
-        <Route path="/user-profile" element={<Layout showHero><UserProfile/></Layout>} />
-        <Route path="*" element={<Navigate to='/'/>} />
+      <Route
+        path="/*"
+        element={
+          <Layout>
+            <Homepage />
+          </Layout>
+        }
+      />
 
+      <Route
+        path="/user-profile"
+        element={
+          <Layout>
+            <UserProfile />
+          </Layout>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
