b3401e3013cb79822a16047fb860aa9bbb03124e
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-11-16 23:48:56 +0530

    validation

--- Backend/src/middleware/validation.ts
+++ Backend/src/middleware/validation.ts
@@ -2,17 +2,18 @@ import { body } from "express-validator";
 import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 
-const handlevalidationErrors = (req:Request, res:Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-}
+const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
 export const validateMyUserRequest = [
-    body('name').isString().notEmpty().withMessage('Name must be string and not empty'),
-    body('addressLine1').isString().notEmpty().withMessage('AddressLine1 must be string and not empty'),
-    body('city').isString().notEmpty().withMessage('City must be string and not empty'),
-    body('country').isString().notEmpty().withMessage('Country must be string and not empty'),
-    handlevalidationErrors,
-]
+  body("name").isString().notEmpty().withMessage("Name must be a string and not empty"),
+  body("addressLine1").isString().notEmpty().withMessage("AddressLine1 must be a string and not empty"),
+  body("city").isString().notEmpty().withMessage("City must be a string and not empty"),
+  body("country").isString().notEmpty().withMessage("Country must be a string and not empty"),
+  handleValidationErrors,
+];
